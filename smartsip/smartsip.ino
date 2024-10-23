e186b3a3fd02bf700b91a6d03a02995e3079e11c
Feng DING <dingbobby@hotmail.com>
2024-10-23 14:50:42 +0800

    update LED flash

--- smartsip/smartsip.ino
+++ smartsip/smartsip.ino
@@ -250,11 +250,11 @@ int setup_sdcard() {
 // LED
 
 void flash_led() {
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 5; i++) {
     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(50);                      // wait for a second
+    delay(200);                      // wait for a second
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-    delay(50); 
+    delay(200); 
   }
 }
 
